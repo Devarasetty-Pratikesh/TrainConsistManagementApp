8904aefe54deb50422e9ba484c18cd682fe072ef
Devarasetty Prethikesh <pratikeshdevarasetty1315@gmail.com>
2026-04-07 11:03:48 +0530

    Update TrainConsistManagementApp.java

--- App/src/TrainConsistManagementApp.java
+++ App/src/TrainConsistManagementApp.java
@@ -1,36 +1,19 @@
 import java.util.LinkedList;
-
 public class TrainConsistManagementApp {
-
-    public static void main(String[] args) {
-
-        // Step 1: Welcome Message
-        System.out.println("=== Train Consist Management App ===");
-
-        // Step 2: Create LinkedList
-        LinkedList<String> trainConsist = new LinkedList<>();
-
-        // Step 3: Add bogies
+   public static void main(String[] args) {
+        System.out.println("=== Train Consist Management App ===");    
+        LinkedList<String> trainConsist = new LinkedList<>();  
         trainConsist.add("Engine");
         trainConsist.add("Sleeper");
         trainConsist.add("AC");
         trainConsist.add("Cargo");
         trainConsist.add("Guard");
-
-        System.out.println("Initial Train: " + trainConsist);
-
-        // Step 4: Insert Pantry Car at position 2
+        System.out.println("Initial Train: " + trainConsist);        
         trainConsist.add(2, "Pantry Car");
-
-        System.out.println("After adding Pantry Car: " + trainConsist);
-
-        // Step 5: Remove first and last bogie
+        System.out.println("After adding Pantry Car: " + trainConsist);       
         trainConsist.removeFirst();
-        trainConsist.removeLast();
-
-        // Step 6: Final Output
+        trainConsist.removeLast();       
         System.out.println("Final Train Consist: " + trainConsist);
-
         System.out.println("Ordered structure maintained successfully.");
     }
 }
